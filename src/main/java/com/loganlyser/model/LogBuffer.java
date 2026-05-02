65a4783865faf82bbbffe49164d23273bad65e33
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-02 18:51:06 +0100

    docs(model): add comments to LogBuffer class

--- src/main/java/com/loganlyser/model/LogBuffer.java
+++ src/main/java/com/loganlyser/model/LogBuffer.java
@@ -3,6 +3,9 @@ package main.java.com.loganlyser.model;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Custom queue class for all IdentifiableLog objects.
+ */
 public class LogBuffer<T extends IdentifiableLog> {
     private Queue<T> queue = new LinkedList<>();
 
@@ -10,10 +13,18 @@ public class LogBuffer<T extends IdentifiableLog> {
         queue.add(log);
     }
 
+    /**
+     * Retrieves and removes the head of queue.
+     * 
+     * @return An object under the IdentifiableLog interface contract.
+     */
     public T nextLog() {
         return queue.poll();
     }
 
+    /**
+     * @return True if queue contains no elements
+     */
     public boolean isEmpty() {
         return queue.isEmpty();
     }
