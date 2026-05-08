907e1562d2f95b184557f11067408da3c4e6e3b6
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-08 15:19:58 +0100

    docs(model): add comments within IdentifiableLog

--- src/main/java/com/loganlyser/model/IdentifiableLog.java
+++ src/main/java/com/loganlyser/model/IdentifiableLog.java
@@ -11,6 +11,11 @@ public interface IdentifiableLog extends Comparable<IdentifiableLog> {
     LogLevel level();
     String message();
     
+    /**
+     * Accesses the unique instance variable.
+     * 
+     * @return A uniquely define instance variable within subclasses.
+     */
     default String getMetaData() {
         return "N/A";
     }
@@ -19,6 +24,13 @@ public interface IdentifiableLog extends Comparable<IdentifiableLog> {
         return String.format("(%s)[%s] %s: %s (Meta: %s)", id(), timestamp(), level(), message(), getMetaData());
     }
 
+    /**
+     * Compares the log levels with the instance log and parsed log.
+     * 
+     * @param other The parsed log
+     * @return The value 0 if instance == parsed; a value less than 0 if 
+     *  instance < parsed; and a value greater than 0 if instance > parsed.
+     */
     @Override
     default int compareTo(IdentifiableLog other) {
         return Integer.compare(this.level().getPriority(), other.level().getPriority());
