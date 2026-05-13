dad22405796cd7ea52702c7255b78ec72473d004
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-13 11:46:42 +0100

    feat(process): add run() method to LogProcessor
    - Change LogProcessor to implement Runnable & override it's run method.
    - Add a constructor to allow for parsing of a LogBuffer instance.
    - Simplify the process method to use the getFormattedEntry method.

--- src/main/java/com/loganlyser/processor/LogProcessor.java
+++ src/main/java/com/loganlyser/processor/LogProcessor.java
@@ -1,13 +1,36 @@
 package main.java.com.loganlyser.processor;
 
+import main.java.com.loganlyser.buffer.LogBuffer;
+import main.java.com.loganlyser.model.EndOfStreamLog;
 import main.java.com.loganlyser.model.IdentifiableLog;
 
-public class LogProcessor<T extends IdentifiableLog> {
-    public void process(T log) {
-        System.out.println("ID: " + log.id());
-        System.out.println("Timestamp: " + log.timestamp());
-        System.out.println("Level: " + log.level());
-        System.out.println("Message: " + log.message());
-        System.out.println("MetaData: " + log.getMetaData());
+public class LogProcessor<T extends IdentifiableLog> implements Runnable{
+    private final LogBuffer<IdentifiableLog> buffer;
+
+    public LogProcessor(LogBuffer<IdentifiableLog> buffer) {
+        this.buffer = buffer;
+    }
+
+    public void process(IdentifiableLog log) {
+        System.out.println("Processing: " + log.getFormattedEntry());
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                IdentifiableLog log = buffer.nextLog();
+
+                if (log instanceof EndOfStreamLog) {
+                    System.out.println("Processor: End of stream reached. Shutting down...");
+                    break;
+                }
+
+                process(log);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Processor interrupted: " + e.getMessage());
+        }
     }
 }
