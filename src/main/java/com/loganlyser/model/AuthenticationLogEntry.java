c1be52b15540a036a32a9c9c34518e55ab31d735
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-05-14 11:57:52 +0100

    feat(model): add AuthenticationLogEntry record

---
+++ src/main/java/com/loganlyser/model/AuthenticationLogEntry.java
@@ -0,0 +1,31 @@
+package main.java.com.loganlyser.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * An immutable representation of a single authentication log event within the system.
+ * 
+ * @param id        The unique identifier for the event.
+ * @param timestamp The exact date and time the event occured.
+ * @param level     The severity level of the event.
+ * @param message   The description text associated with the log.
+ * @param ipAddress The ip address of the where the event occured.
+ * @see IdentifiableLog The interface for log event records.
+ */
+public record AuthenticationLogEntry(
+        String id,
+        LocalDateTime timestamp,
+        LogLevel level,
+        String message,
+        String ipAddress) implements IdentifiableLog {
+
+    @Override
+    public String getFormattedEntry() {
+        return String.format("(%s) -NETWORK- [%s] %s: %s (IP Address: %s)", id, timestamp, level, message, ipAddress);
+    }
+
+    @Override
+    public String getMetaData() {
+        return ipAddress;
+    }
+}
